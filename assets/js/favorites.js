63e1adfbf2ebbdf744ef71c17dfddad347a6fbdd
JoelAbankwah <100015338+JoelAbankwah@users.noreply.github.com>
2022-04-16 12:36:43 -0700

    add ability to remove facts and imgs on click

--- assets/js/favorites.js
+++ assets/js/favorites.js
@@ -23,3 +23,29 @@ function getFacts(){
     };
 getPhotos()
 getFacts()
+
+var removeFact = function(event) {
+    var savedFacts = JSON.parse(localStorage.getItem('savedfacts'))
+    for (var i=0; i<savedFacts.Text.length; i++) {
+        if (event.target.textContent === savedFacts.Text[i]) {
+            savedFacts.Text.splice(i, 1);
+        }
+    }
+    localStorage.setItem('savedfacts', JSON.stringify(savedFacts))
+    document.location.reload();
+}
+
+var removeImg = function(event) {
+    var savedPhotos = JSON.parse(localStorage.getItem('savedphotos'))
+    console.log(event.target.src)
+    for (var i=0; i<savedPhotos.Url.length; i++) {
+        if (event.target.src === savedPhotos.Url[i]) {
+            savedPhotos.Url.splice(i, 1);
+        }
+    }
+    localStorage.setItem('savedphotos', JSON.stringify(savedPhotos))
+    document.location.reload();
+}
+
+displayEl.addEventListener('click', removeImg)
+factEl.addEventListener('click', removeFact)
