410fcca2f3aee4a263f089aac15a80ff556f24e1
Akeva Melchor <akeva.melchor@expel.io>
2022-04-18 20:52:58 -0700

    favorite styling

--- assets/js/favorites.js
+++ assets/js/favorites.js
@@ -8,9 +8,9 @@ var deleteBtn = document.querySelector('#delete-photo')
 var deleteFactBtn = document.querySelector('#delete-fact')
 var openFullBtn = document.querySelector("#open")
 
-function getPhotos(){
-var favoritePhotos = JSON.parse(localStorage.getItem('savedphotos'))
-    for (var i=0; i<favoritePhotos.Url.length; i++) {
+function getPhotos() {
+    var favoritePhotos = JSON.parse(localStorage.getItem('savedphotos'))
+    for (var i = 0; i < favoritePhotos.Url.length; i++) {
         var image = document.createElement("img");
         image.setAttribute("src", favoritePhotos.Url[i]);
         image.setAttribute('class', 'fav-imgs');
@@ -20,15 +20,15 @@ var favoritePhotos = JSON.parse(localStorage.getItem('savedphotos'))
     }
 };
 
-function getFacts(){
+function getFacts() {
     var savedFacts = JSON.parse(localStorage.getItem('savedfacts'))
-        for (var i=0; i<savedFacts.Text.length; i++) {
-            var factText = document.createElement("p");
-            factText.setAttribute('class', 'facts primary callout')
-            factText.textContent = savedFacts.Text[i];
-            factEl.appendChild(factText);
-        }
-    };
+    for (var i = 0; i < savedFacts.Text.length; i++) {
+        var factText = document.createElement("p");
+        factText.setAttribute('class', 'fav-img facts primary callout')
+        factText.textContent = savedFacts.Text[i];
+        factEl.appendChild(factText);
+    }
+};
 getPhotos()
 getFacts()
 
@@ -38,17 +38,17 @@ var DisplayFactModal = function(event) {
     localStorage.setItem('currentfact', JSON.stringify(currentFact))
 }
 
-var removeFact = function(){
+var removeFact = function() {
     var savedFacts = JSON.parse(localStorage.getItem('savedfacts'))
     var currentFact = JSON.parse(localStorage.getItem('currentfact'));
 
-    for (var i=0; i<savedFacts.Text.length; i++) {
+    for (var i = 0; i < savedFacts.Text.length; i++) {
         if (currentFact === savedFacts.Text[i]) {
             savedFacts.Text.splice(i, 1);
         }
     }
     localStorage.setItem('savedfacts', JSON.stringify(savedFacts))
-    document.location.reload(); 
+    document.location.reload();
 }
 
 var displayPhotoModal = function(event) {
@@ -61,7 +61,7 @@ var removePhoto = function() {
     var savedPhotos = JSON.parse(localStorage.getItem('savedphotos'))
     var currentImg = JSON.parse(localStorage.getItem('currentimg'))
 
-    for (var i=0; i<savedPhotos.Url.length; i++) {
+    for (var i = 0; i < savedPhotos.Url.length; i++) {
         if (currentImg === savedPhotos.Url[i]) {
             savedPhotos.Url.splice(i, 1);
         }
@@ -70,7 +70,7 @@ var removePhoto = function() {
     document.location.reload();
 }
 
-var openImg = function(){
+var openImg = function() {
     var currentImg = JSON.parse(localStorage.getItem('currentimg'))
     document.location = currentImg
 }
@@ -82,13 +82,13 @@ deleteFactBtn.addEventListener('click', removeFact)
 openFullBtn.addEventListener('click', openImg)
 span.onclick = function() {
     photoModal.style.display = "none";
-  }
-  span1.onclick = function() {
+}
+span1.onclick = function() {
     factModal.style.display = "none";
-  }
-  window.onclick = function(event) {
+}
+window.onclick = function(event) {
     if (event.target == photoModal || event.target == factModal) {
-      photoModal.style.display = "none";
-      factModal.style.display = "none";
+        photoModal.style.display = "none";
+        factModal.style.display = "none";
     }
-  }
+}
